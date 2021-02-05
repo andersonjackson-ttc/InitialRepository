4aabf62ec27db1200480f25752a6992583895c7a
Gamerbro3333 <77407944+Gamerbro3333@users.noreply.github.com>
2021-02-05 16:54:50 -0500

    Update startup.js

--- startup.js
+++ startup.js
@@ -1,3 +1,6 @@
-function ahj() {
-    document.write("anderson was here");
+function myFunction() {
+  var x = document.createElement("BUTTON");
+  var t = document.createTextNode("Click me");
+  x.appendChild(t);
+  document.body.appendChild(x);
 }
