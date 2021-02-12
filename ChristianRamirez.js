93bd628784a7a79023be0856b935f1f013af50a0
ChristianRamirezGutierrez <75754083+ChristianRamirezGutierrez@users.noreply.github.com>
2021-02-11 20:05:18 -0500

    Description of ChristianRamirez.js
    Should add some new text to the project and changes the background color to green.

---
+++ ChristianRamirez.js
@@ -0,0 +1,8 @@
+function testCR() {
+    document.write("<br><br>I wanted to check if this works!");
+    document.write("<br>So I moved text into this function");
+}
+
+function myFunction() {
+  document.body.style.backgroundColor = "green";
+}
