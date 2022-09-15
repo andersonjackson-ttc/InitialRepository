ec415070501f2e4de3a3e21e8999b2efb8e28959
Cesar Garcia <cesargarcia9046@gmail.com>
2022-09-14 21:41:45 -0400

    Update CGarcia.js
    Added a comment that describes the purpose of the "Dont click me" button

--- CGarcia.js
+++ CGarcia.js
@@ -1,3 +1,4 @@
+//A simple button that when clicked on displays an alert with the text "Howdy, Cesar made this one"
 function cBtn() {
     document.write("<br>")
     let btn = document.createElement("button");
