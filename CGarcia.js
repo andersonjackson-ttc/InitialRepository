2c6619faf3259c3d35446387c925cc42947d52d3
Cesar Garcia <cesargarcia9046@gmail.com>
2022-09-14 21:32:33 -0400

    updates

--- CGarcia.js
+++ CGarcia.js
@@ -1,6 +1,11 @@
 function cBtn() {
+    document.write("<br>")
     let btn = document.createElement("button");
     btn.innerHTML = "Don't Click Me";
     document.body.appendChild(btn);
+
+    btn.addEventListener("click", function(){
+        alert("Howdy, Cesar made this one");
+    })
 
 }
