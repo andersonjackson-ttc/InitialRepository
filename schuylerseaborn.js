b9d2f7253032234feec8a6cc3bcc625a943e4de9
wsseabor <64116036+wsseabor@users.noreply.github.com>
2022-09-18 20:02:33 -0400

    Added schuylerseaborn.js
    Added personal .js file and edited demo.html to reflect

---
+++ schuylerseaborn.js
@@ -0,0 +1,10 @@
+function draw() {
+    var canvas = document.getElementById("canv")
+    var ctx = canvas.getContext("2d")
+    ctx.fillStyle = "#FF0000"
+    ctx.fillRect(0, 0, 150, 75)
+}
+
+function reply() {
+    document.write("So was Schuyler Seaborn")
+}
