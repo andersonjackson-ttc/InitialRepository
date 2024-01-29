7c652e48d7da78b6057164ac43969c0811403f6c
CharlesHarrington-Developer <78627600+CharlesHarrington-Developer@users.noreply.github.com>
2024-01-29 13:53:00 -0500

    Formatting change
    I did not format my code prior to first commit.  Functionality remains the same.

--- CHarrington.js
+++ CHarrington.js
@@ -7,18 +7,18 @@
  */
 
 function displayRandomQuote() {
-    const quotes = [
-        "I can do all things through Christ which strengtheneth me. - Philippians 4:13",
-        "CPT275: Where our code doesn't always work, but 'our CTRL-Z' skills are impeccable. - Chuck H.",
-        "The only way to do great work is to love what you do. - Steve Jobs",
-        "The journey of a thousand miles begins with one step. - Lao Tzu",
-        "That which does not kill us makes us stronger. - Friedrich Nietzsche",
-        "Life is what happens when you are busy making other plans. - John Lennon",
-        "When the going gets tough, the tough get going. - John Kennedy"
-    ];
+  const quotes = [
+    "I can do all things through Christ which strengtheneth me. - Philippians 4:13",
+    "CPT275: Where our code doesn't always work, but 'our CTRL-Z' skills are impeccable. - Chuck H.",
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "The journey of a thousand miles begins with one step. - Lao Tzu",
+    "That which does not kill us makes us stronger. - Friedrich Nietzsche",
+    "Life is what happens when you are busy making other plans. - John Lennon",
+    "When the going gets tough, the tough get going. - John Kennedy",
+  ];
 
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    const randomQuote = quotes[randomIndex];
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
 
-    document.getElementById("quoteDisplay").innerText = randomQuote;
+  document.getElementById("quoteDisplay").innerText = randomQuote;
 }
