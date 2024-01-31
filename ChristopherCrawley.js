6062178e2fceb25580b258ca8b662ce2ff261f9f
CCrawley5979 <157995959+CCrawley5979@users.noreply.github.com>
2024-01-31 15:37:19 -0500

    Update ChristopherCrawley.js

--- ChristopherCrawley.js
+++ ChristopherCrawley.js
@@ -1,3 +1,10 @@
-function chrisfunction(a, b) {
-  return a + b;
+function chrisfunction(number) {
+  return number % 2 === 0;
 }
+
+// Example usage:
+let num1 = 10;
+let num2 = 7;
+
+console.log(chrisfunction(num1));
+console.log(chrisfunction(num2));
