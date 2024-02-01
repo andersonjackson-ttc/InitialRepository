d231d2427857400c1ad84e3380583cdc6528db96
RhyderSease <158112056+RhyderSease@users.noreply.github.com>
2024-02-01 15:34:08 -0500

    Add files via upload
    This button spawns The Spot in as many times as you want

---
+++ RhyderSease.js
@@ -0,0 +1,9 @@
+// RhyderSease.js
+
+function theImage() {
+    var spotButton = document.getElementById("spot");
+    var spotImage = document.createElement("img");
+    spotImage.src = "https://static.wikia.nocookie.net/vsbattles/images/6/63/Spot.png/revision/latest/scale-to-width-down/280?cb=20230924184603";
+    spotImage.alt = "Spot Image";
+    spotButton.appendChild(spotImage);
+  }
