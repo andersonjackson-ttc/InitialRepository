f62e5fc80d04c64b65b1ef39dfcf4c0e36600c7f
aitaTchakounte <157419029+aitaTchakounte@users.noreply.github.com>
2024-02-01 04:31:38 -0500

    Update AitaTchakounte.js

--- AitaTchakounte.js
+++ AitaTchakounte.js
@@ -1,31 +1,10 @@
-// Define a JavaScript function named 'changeColors'
-function changeColors() {
-    // Get the value entered in the text input field with ID 'color-input'
-    var color = document.getElementById("color-input").value;
+function AitaFunction() {
+    const AitaButton = document.getElementById('yourButton');
 
-    // Get the reference to the checkbox element with ID 'font-color-checkbox'
-    var checkbox = document.getElementById("font-color-checkbox");
-
-    // Change the background color of the page to the entered color
-    document.body.style.backgroundColor = color;
-
-    // Check if the checkbox is checked
-    if (checkbox.checked) {
-        // If checked, change the font color to white
-        document.body.style.color = "white";
-    } else {
-        // If not checked, revert to default font color
-        document.body.style.color = "black";
-    }
-}
-
-// Call the function when the HTML document is fully loaded
-document.addEventListener("DOMContentLoaded", function() {
-    // Get the reference to the button element with ID 'color-button'
-    var button = document.getElementById("color-button");
-
-    // Add event listener to the button to call the 'changeColors' function when clicked
-    button.addEventListener("click", function() {
-        changeColors();
+    AitaButton.addEventListener('click', function() {
+        document.getElementById('AitaHeading').innerText = "Clicked!";
     });
-});
+
+    document.getElementById("AitaHeading").style.color = "pink";
+    document.getElementById("AitaHeading").style.fontSize = "60px";
+}
